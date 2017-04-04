d74b7617f3877d905956a66d2dc032c897d5aaea
Juan Edwards <jotaedw@MacBook-Pro-de-Juan.local>
2017-04-04 20:40:54 -0300

    Weedbook App - Agragada la opcion de registro y de login con sus respectivas vistas

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -17,6 +17,16 @@ Route::controllers([
  	'auth' => 'Auth\AuthController',
  	'password' => 'Auth\PasswordController',
 ]); //auth
+
+// Authentication routes...
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/logout', 'Auth\AuthController@getLogout');
+
+// Registration routes...
+Route::get('auth/register', 'Auth\AuthController@getRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');
+
 Route::group(['middleware' => ['auth']], function()
 {
 	Route::get('new-review', 'ReviewController@create');
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -25,7 +25,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="http://www.findalltogether.com">Find All Together</a>
+          <a class="navbar-brand" href="http://www.weedbook.com">WeedBook</a>
         </div>
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
           <ul class="nav navbar-nav">
@@ -45,7 +45,7 @@
             <li class="dropdown">
               <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
               <ul class="dropdown-menu" role="menu">
-                @if (Auth::user()->can_post())
+                @if (!Auth::guest())
                 <li>
                   <a href="{{ url('/new-post') }}">Add new post</a>
                 </li>
@@ -100,7 +100,7 @@
       </div>
       <div class="row">
         <div class="col-md-10 col-md-offset-1">
-          <p>Copyright © 2015 | <a href="http://www.findalltogether.com">Find All Together</a></p>
+          <p>Copyright © 2017 | <a href="http://www.weedbook.com">WeedBook</a></p>
         </div>
       </div>
     </div>
