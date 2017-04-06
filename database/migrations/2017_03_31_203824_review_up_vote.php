e172fc4fc202184524fd935ed20fcddc274c5984
Paul Beltran <p.beltranes@gmail.com>
2017-04-05 23:40:47 -0300

    arreglando migraciones ahora si estan la raja

--- database/migrations/2017_03_31_203824_review_up_vote.php
+++ database/migrations/2017_03_31_203824_review_up_vote.php
@@ -12,7 +12,7 @@ class ReviewUpVote extends Migration
      */
     public function up()
     {
-        Schema::create('review_up_vote', function (Blueprint $table) {
+        Schema::create('review_up_votes', function (Blueprint $table) {
           $table->increments('id');
           $table -> integer('user_id') -> unsigned() -> default(0);
           $table -> integer('review_id') -> unsigned() -> default(0);
@@ -29,6 +29,6 @@ class ReviewUpVote extends Migration
      */
     public function down()
     {
-        Schema::drop('review_up_vote');
+        Schema::drop('review_up_votes');
     }
 }
