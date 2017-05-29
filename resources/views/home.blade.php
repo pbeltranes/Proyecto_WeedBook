5033849f398e1468055d8ecab1f57fd2c71c57a2
Paul Beltran <p.beltranes@gmail.com>
2017-05-29 09:16:34 -0400

    Actualización view myreviews

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,16 +13,16 @@ There is no reviews till now. Login and write a new post now!!!
 ?>
 
 <div class="w3-row-padding">
-  @foreach( $reviews as $post )
-  <?php $back = $post->background_image_url ? $post->background_image_url : 'http://www.acnur.org/fileadmin/Images/ACNUR/noticias/2016/Octubre_2016/5809ae163.jpg';  ?>
+  @foreach( $reviews as $review )
+  <?php $back = $review->background_image_url ? $review->background_image_url : 'http://www.acnur.org/fileadmin/Images/ACNUR/noticias/2016/Octubre_2016/5809ae163.jpg';  ?>
     <div class="w3-third w3-container w3-margin-bottom">
-    <a href='{{url("/review/".$post->id)}}'>  <img src="{{$back}}" alt="Norway" style="border-radius: 50%;
+    <a href='{{url("/review/".$review->id)}}'>  <img src="{{$back}}" alt="Norway" style="border-radius: 50%;
         overflow: hidden;
         width: 150px;
         height: 150px;" class="w3-hover-opacity"> </a>
       <div class="w3-container w3-white">
         <p>  </p>
-        <p><b>{{$post->title}}</b></p>
+        <p><b>{{$review->title}}</b></p>
         <p>Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.</p>
       </div>
     </div>
