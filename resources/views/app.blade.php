c821d9fd351c42ed694d97c601b310ebd74dc61e
Paul Beltran <p.beltranes@gmail.com>
2017-06-28 21:42:10 -0400

    retoques

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -26,7 +26,7 @@
     @include('auth/registermodal')
     @include('reviews/newreviewmodal')
     <nav class="navbar navbar-default">
-      <div class="container-fluid soft-greenb">
+      <div class="container-fluid ">
         <div class="navbar-header ">
           <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
             <span class="sr-only">Toggle Navigation</span>
